0de1cc91e3ccabd4ad092ef9a3706d07d3eccbc8
JakeSloan <sloanja3@msu.edu>
2022-11-20 22:59:19 -0500

    comit

---
+++ soundboard.js
@@ -0,0 +1,9 @@
+
+  var buttonElement = document.getElementById('first')
+
+  var johnCena = audio ('johnCena.mp3')
+
+  buttonElement.addEventListener('click', function () {
+    this.onclick()
+  })
+
